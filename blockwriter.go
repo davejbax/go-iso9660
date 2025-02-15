8aff364647ff02ee598fbcf49a68b78603d6359d
davejbax <davejbax@gmail.com>
2025-02-15 16:46:51 +0000

    Implement block writer

--- blockwriter.go
+++ blockwriter.go
@@ -1,21 +1,58 @@
 package iso9660
 
-import "io"
+import (
+	"errors"
+	"fmt"
+	"github.com/itchio/headway/counter"
+	"io"
+)
 
 type blockWriter struct {
-	wrapped io.Writer
+	wrapped *counter.Writer
 
 	currentBlock uint32
 }
 
+var errNonSequentialBlockWrite = errors.New("cannot write blocks in non-sequential order or rewrite existing blocks")
+
 func newBlockWriter(wrapped io.Writer) *blockWriter {
-	return &blockWriter{wrapped: wrapped}
+	return &blockWriter{wrapped: counter.NewWriter(wrapped)}
 }
 
-func (*blockWriter) WriteBlock(number uint32, contents io.WriterTo) error {
+func (w *blockWriter) WriteBlock(number uint32, contents io.WriterTo) error {
+	if number < w.currentBlock {
+		return errNonSequentialBlockWrite
+	}
+
+	zeroBlock := make([]byte, logicalBlockSize)
+	for number > w.currentBlock {
+		if _, err := w.wrapped.Write(zeroBlock); err != nil {
+			return fmt.Errorf("failed to write padding prior to block: %w", err)
+		}
+
+		w.currentBlock += 1
+	}
+
+	contentsSize, err := contents.WriteTo(w.wrapped)
+	if err != nil {
+		return fmt.Errorf("failed to write contents to block: %w", err)
+	}
+
+	contentsBlocks := (contentsSize + logicalBlockSize - 1) / logicalBlockSize
+
+	if contentsBlocks*logicalBlockSize > contentsSize {
+		// Note: this should never be more than a block in size being allocated here
+		padding := make([]byte, contentsBlocks*logicalBlockSize-contentsSize)
+		if _, err := w.wrapped.Write(padding); err != nil {
+			return fmt.Errorf("failed to write padding after contents: %w", err)
+		}
+	}
+
+	w.currentBlock += uint32(contentsBlocks)
+
 	return nil
 }
 
-func (*blockWriter) BytesWritten() int64 {
-	return 0
+func (w *blockWriter) BytesWritten() int64 {
+	return w.wrapped.Count()
 }
--- blockwriter_test.go
+++ blockwriter_test.go
@@ -19,7 +19,7 @@ func TestBlockWriter_WriteBlock(t *testing.T) {
 	err := bw.WriteBlock(0x16, testBlock16)
 	require.NoError(t, err, "WriteBlock should not return an error for valid input")
 
-	assert.Equal(t, 0x16*2048+testBlock16.Len(), buff.Len(), "WriteBlock should write all prior zeros and given block, and no more")
+	assert.Equal(t, 0x17*2048, buff.Len(), "WriteBlock should write all prior zeros and given block, plus padding to make whole number of blocks")
 	assert.EqualValues(t, buff.Len(), bw.BytesWritten(), "BytesWritten should return correct count after writing one block")
 	assert.Equal(t, make([]byte, 0x16*2048), buff.Bytes()[:0x16*2048], "WriteBlock should write zeros prior to first block")
 	assert.Error(t, bw.WriteBlock(0x16, testBlock16), "WriteBlock should not allow rewriting the same block")
@@ -34,7 +34,7 @@ func TestBlockWriter_WriteBlock(t *testing.T) {
 	err = bw.WriteBlock(0x17, testBlock17To18)
 	require.NoError(t, err, "WriteBlock should not return an error for valid input on second block")
 
-	assert.Equal(t, 0x17*2048+len(testBlock17To18Data), buff.Len(), "WriteBlock should write correct number of bytes when given more than one block of data to write")
+	assert.Equal(t, 0x19*2048, buff.Len(), "WriteBlock should write correct number of bytes when given more than one block of data to write, and pad to number of whole blocks")
 	assert.EqualValues(t, buff.Len(), bw.BytesWritten(), "BytesWritten should return correct count after writing three blocks")
 	assert.Equal(t, testBlock17To18Data, buff.Bytes()[0x17*2048:0x18*2048+100], "WriteBlock should write correct data when given more than one block of data to write")
 	assert.Error(t, bw.WriteBlock(0x18, testBlock16), "WriteBlock should not allow rewriting a written block when given more than one block of data to write")
